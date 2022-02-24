4fed0f15fa852337e6c82f3b3568bb0143ac0c9f
ydc_glb <goodMoring@qq.com>
2022-02-24 13:10:47 +0800

    first code

---
+++ src/main/java/com/atguigu.java
@@ -0,0 +1,12 @@
+package com;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * @author ydc
+ */
+public class atguigu {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
