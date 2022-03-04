005c1a882a60b21c339739d0fd7c63c7ad5efefa
ydc_glb <goodMoring@qq.com>
2022-03-04 15:33:42 +0800

    first first commit

--- src/main/java/com/atguigu.java
+++ src/main/java/com/atguigu.java
@@ -8,9 +8,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 public class atguigu {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("第一次提交");
 
     }
 }
